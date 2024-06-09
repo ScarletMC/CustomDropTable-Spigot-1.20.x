1d155e5f26a50b7daba665522c02a4f3cceffdd9
SirImperivm <134490705+SirImperivm@users.noreply.github.com>
2024-06-09 11:24:00 +0200

    Changelog: - Inserimento configurazioni; - Inserimento sistema di errori.
    Upload Previsti:
    - Inserimento modificatore incantesimi;
    - Inserimento comando admin;
    - Inserimento morte dei mob.

--- src/main/java/me/sirimperivm/spigot/Main.java
+++ src/main/java/me/sirimperivm/spigot/Main.java
@@ -1,6 +1,8 @@
 package me.sirimperivm.spigot;
 
+import me.sirimperivm.spigot.utils.ConfigManager;
 import me.sirimperivm.spigot.utils.colors.Colors;
+import me.sirimperivm.spigot.utils.other.Errors;
 import me.sirimperivm.spigot.utils.other.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -10,12 +12,16 @@ public final class Main extends JavaPlugin {
     private Main plugin;
     private Colors colors;
     private Logger log;
+    private ConfigManager configManager;
+    private Errors errors;
 
     @Override
     public void onEnable() {
         plugin = this;
         colors = new Colors(plugin);
         log = new Logger(plugin, "CustomDropTable");
+        configManager = new ConfigManager(plugin);
+        errors = new Errors(plugin);
 
         log.success("Plugin attivato correttamente.");
     }
@@ -35,5 +41,13 @@ public final class Main extends JavaPlugin {
 
     public Logger getLog() {
         return log;
+    }
+
+    public ConfigManager getConfigManager() {
+        return configManager;
+    }
+
+    public Errors getErrors() {
+        return errors;
     }
 }
